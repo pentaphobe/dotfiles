c0f4b05b0e6f64847876488ebd136157edca9c3d
Keilin Olsen <keilin@vectorflip.com>
2018-11-24 15:55:02 +1100

    temporarily disable fancy prompt

---
+++ home/prompt.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Work in progress
+#export PS1="\[$(tput bold)\]\[$(tput setaf 6)\]\[$(tput setaf 3)\]\u\[$(tput setaf 1)\]@\[$(tput setaf 3)\]\[$(tput setaf 6)\]\[$(nicePath "\w")\]\[$(tput setaf 4)\]\\$ \[$(tput sgr0)\]"
+
