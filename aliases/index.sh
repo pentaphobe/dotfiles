cacb3196873cab55ad10e43dd9be4d7a08a2c849
Keilin Olsen <keilin@vectorflip.com>
2018-11-24 15:48:15 +1100

    initial pass of path-nicener
    - preserves most and least significant paths
    - truncates the middle
    - currently doesn't work properly with names containing spaces
    - tty cursor isn't held properly

--- aliases/index.sh
+++ aliases/index.sh
@@ -11,3 +11,22 @@ function setHostname() {
   sudo scutil --set LocalHostName ${name}.local
   sudo scutil --set ComputerName ${name}
 }
+
+MAX_PATH_LEN=20
+
+function fixHome() {
+ echo $(sed -e "s|${HOME}|~|g" <<< "${1}")
+}
+
+function nicePath() {
+  local foo=$(fixHome $1)  
+  local len=${#foo}
+  local parts
+  if [ $len -ge $MAX_PATH_LEN ]; then
+    IFS='/' read -r -a parts <<< "$foo"
+    local partsLen=${#parts[@]}
+    echo "${parts[0]}/.../${parts[$partsLen-2]}/${parts[$partsLen-1]}"    
+  else
+    echo $foo
+  fi
+} 
