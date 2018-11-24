3d29878c2a616cd4e484946e643af1cfe729310d
Keilin Olsen <keilin@vectorflip.com>
2018-11-24 15:45:48 +1100

    add helper for setting hostname (macOS)

--- aliases/index.sh
+++ aliases/index.sh
@@ -3,3 +3,11 @@
 alias ls='ls -G'
 
 function gi() { curl -L -s https://www.gitignore.io/api/$@ ;}
+
+function setHostname() {
+  local name=$1
+
+  sudo scutil --set HostName ${name}
+  sudo scutil --set LocalHostName ${name}.local
+  sudo scutil --set ComputerName ${name}
+}
